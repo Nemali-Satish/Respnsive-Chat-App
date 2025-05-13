4572d533e9579166ea63d44e604c3d146a7fdd34
Satish Nemali <satishnemali2004@gmail.com>
2025-05-13 22:48:50 +0530

    Modified Server Files

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
