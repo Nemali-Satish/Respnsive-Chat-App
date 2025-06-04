a505059b35457af3e02b51dcb314ad37ec291f41
Satish Nemali <satishnemali2004@gmail.com>
2025-06-04 14:03:43 +0530

    Revert "Implemnted Filter Online Users Feature"
    This reverts commit 9ed7000e5cf9151617a116b0ef620e4957c8ae80.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,7 +3,6 @@ import { useChatStore } from "../store/useChatStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { User } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore";
-import { useState } from "react";
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } =
@@ -15,13 +14,7 @@ const Sidebar = () => {
 
   const { onlineUsers } = useAuthStore();
 
-  const [showOnlineOnly, setShowOnlineOnly] = useState(false);
-
   if (isUsersLoading) return <SidebarSkeleton />;
-
-  const filteredUsers = showOnlineOnly
-    ? users.filter((user) => onlineUsers.includes(user._id))
-    : users;
   return (
     <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
       <div className="border-b border-base-300 w-full p-5">
@@ -29,23 +22,9 @@ const Sidebar = () => {
           <User className="size-6" />
           <span className="font-medium hidden lg:block">Contacts</span>
         </div>
-        <div className="mt-3 hidden  lg:flex items-center gap-2">
-          <label className="cursor-pointer flex items-center gap-2">
-            <input
-              type="checkbox"
-              checked={showOnlineOnly}
-              onChange={(e) => setShowOnlineOnly(e.target.checked)}
-              className="checkbox checkbox-sm"
-            />
-            <span className="text-sm">Show Online Only</span>
-          </label>
-          <span className="text-sm text-zinc-500">
-            ({onlineUsers.length - 1} Online)
-          </span>
-        </div>
       </div>
       <div className="overflow-y-auto w-full py-3">
-        {filteredUsers.map((user) => (
+        {users.map((user) => (
           <button
             key={user._id}
             onClick={() => {
@@ -85,7 +64,7 @@ const Sidebar = () => {
           </button>
         ))}
 
-        {filteredUsers.length === 0 && (
+        {users.length === 0 && (
           <div className="text-center text-zinc-500 py-4">No online users</div>
         )}
       </div>
